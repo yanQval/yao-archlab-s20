68301c35b4508b59b026d7ca70a5cfca92b7230e
Yiwei Li <leepoly2@gmail.com>
2020-01-04 12:42:11 +0800

    [parse_pixel.py]: remove numpy dependency

--- lab1/run/parse_pixel.py
+++ lab1/run/parse_pixel.py
@@ -2,7 +2,6 @@ try:
     from PIL import Image
 except ImportError:
     import Image
-import numpy as np
 import sys
 
 if len(sys.argv) < 2:
@@ -14,18 +13,18 @@ meta_line = file.readline()
 meta_arr = meta_line.split(' ')
 height = int(meta_arr[0])
 width = int(meta_arr[1])
-data = np.zeros((height, width), dtype=np.uint8)
 
+img = Image.new('L', (width, height)) # Read a grayscale image
+data = img.load()
 i = 0
 for line in file:
     pixel_arr = line.split(' ')
     for j in range(width):
-        data[i, j] = int(pixel_arr[j])
+        data[j, i] = int(pixel_arr[j]) # img data is put in [width_j, height_i] order in PIL
     i = i + 1
 
-img = Image.fromarray(data, 'L')
+img.show()
 outputfile = "image-out.jpg"
 if len(sys.argv) > 2:
     outputfile = sys.argv[2]
 img.save(outputfile)
-img.show()
